3f40a56d83a9cc4db9c341c37b87b228cef888c6
Artemis77u <WinnerNow777@proton.me>
2025-05-29 21:37:17 +0530

    Update airtable-proxy.js

--- api/airtable-proxy.js
+++ api/airtable-proxy.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
   const { video } = req.query;
 
-  const response = await fetch(`https://api.airtable.com/v0/YOUR_BASE_ID/YOUR_TABLE_NAME?view=Published&sort[0][field]=Lesson%20Number&sort[0][direction]=asc`, {
+  const response = await fetch(`https://api.airtable.com/v0/app0nqXjYS3mdULGj/Videos?view=Published&sort[0][field]=Lesson%20Number&sort[0][direction]=asc`, {
     headers: {
-      Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
+      Authorization: `Bearer ${process.env.patNGrZNwfEVXAAQQ.0b066f675070d9280cac1ff726847edb1474cbc9dae0cc8634dd5e2369cac9ae}`
     }
   });
 
